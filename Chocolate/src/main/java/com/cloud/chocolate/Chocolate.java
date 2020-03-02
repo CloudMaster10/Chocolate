b8ff687cd21fdbbfe47882768ce492a3b4f3116e
Cloud <15125@10.122.153.2>
2020-03-01 23:13:53 -0600

    Added Soul Sandstone

--- Chocolate/src/main/java/com/cloud/chocolate/Chocolate.java
+++ Chocolate/src/main/java/com/cloud/chocolate/Chocolate.java
@@ -3,6 +3,7 @@ package com.cloud.chocolate;
 import net.minecraft.block.Block;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -14,49 +15,63 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("chocolate")
-public class Chocolate {
+public class Chocolate
+{
 	
     // Directly reference a log4j logger.
     //private static final Logger LOGGER = LogManager.getLogger();
+	public static final String MOD_ID = "chocolate";
+	public static Chocolate instance;
 
-    public Chocolate() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    public Chocolate()
+    {
+    	final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    	modEventBus.addListener(this::setup);
+    	modEventBus.addListener(this::enqueueIMC);
+    	modEventBus.addListener(this::processIMC);
+    	modEventBus.addListener(this::doClientStuff);
+        
+        instance = this;
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
+    private void setup(final FMLCommonSetupEvent event)
+    {
         // some preinit code
     }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
+    private void doClientStuff(final FMLClientSetupEvent event)
+    {
         // do something that can only be done on the client
     }
 
-    private void enqueueIMC(final InterModEnqueueEvent event) {
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
         // dispatch IMC to another mod
     }
 
-    private void processIMC(final InterModProcessEvent event) {
+    private void processIMC(final InterModProcessEvent event)
+    {
         // receive and process InterModComms from other mods
     }
     
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
+    public void onServerStarting(FMLServerStartingEvent event)
+    {
         // do something when the server starts
     }
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
     // Event bus for receiving Registry Events)
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
+    public static class RegistryEvents
+    {
         @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent)
+        {
             // register a new block here
         }
     }
---
+++ Chocolate/src/main/java/com/cloud/chocolate/blocks/ModStairsBlock.java
@@ -0,0 +1,12 @@
+package com.cloud.chocolate.blocks;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.StairsBlock;
+
+public class ModStairsBlock extends StairsBlock
+{
+	public ModStairsBlock(BlockState state, Properties properties)
+	{
+		super(state, properties);
+	}
+}
